6f78c0c2111aa92f6d0a21da29e9b9b53491e82e
Don Ward <10is0420@gmail.com>
2018-12-29 16:42:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
-  def count_sentences
+  def count_sentences()
+    self.count_sentences.split(//)
+    #binding.pry
+    #binding.pry
 
   end
+  
 end
